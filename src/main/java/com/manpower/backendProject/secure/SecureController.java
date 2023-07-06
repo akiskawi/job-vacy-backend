5aac5899d466f42dfab54e17943a2fb148aafdcd
John Poikilidis <akiskawi@gmail.com>
2023-07-07 01:06:35 +0300

    secure endpoint/login/register created

---
+++ src/main/java/com/manpower/backendProject/secure/SecureController.java
@@ -0,0 +1,15 @@
+package com.manpower.backendProject.secure;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/secure")
+public class SecureController {
+    @GetMapping
+    public ResponseEntity<String> secure(){
+        return ResponseEntity.ok("Secure endpoint!");
+    }
+}
---
+++ src/main/java/com/manpower/backendProject/user/Role.java
@@ -0,0 +1,6 @@
+package com.manpower.backendProject.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
