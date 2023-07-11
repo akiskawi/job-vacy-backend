1369340a3f36417d578c5db1f1df51348aece921
John Poikilidis <akiskawi@gmail.com>
2023-07-11 13:12:31 +0300

    test controller frontend

--- src/main/java/com/manpower/backendProject/test/Test.java
+++ src/main/java/com/manpower/backendProject/test/Test.java
@@ -1,6 +1,7 @@
 package com.manpower.backendProject.test;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class Test {
     @GetMapping
     public String s(){
