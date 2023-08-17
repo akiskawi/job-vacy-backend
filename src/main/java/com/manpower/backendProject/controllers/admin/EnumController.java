0c20a72391684f7f186d609a57c65782535cfee9
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-08-17 16:36:53 +0300

    Leave Request Enum Endpoint Added

---
+++ src/main/java/com/manpower/backendProject/controllers/admin/EnumController.java
@@ -0,0 +1,26 @@
+package com.manpower.backendProject.controllers.admin;
+
+import com.manpower.backendProject.models.leave.LeaveRequestTYPE;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/")
+@CrossOrigin(origins = "http://localhost:3000")
+public class EnumController {
+
+    @GetMapping("leavetypes")
+    public ResponseEntity<Map<String, String>> getLeaveTypes() {
+        Map<String, String> leaveTypes = new HashMap<>();
+
+        for (LeaveRequestTYPE type : LeaveRequestTYPE.values()) {
+            leaveTypes.put(type.name(), type.getLabel());
+        }
+        return ResponseEntity.ok(leaveTypes);
+    }
+}
